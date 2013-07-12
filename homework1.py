a05829e04e7ab196f8ffa0160328a0d951811607
Anna Trostnikova <ankatrostnikova@yahoo.com>
2013-07-12 18:00:30 +0100

    first homework

---
+++ homework1.py
@@ -0,0 +1,55 @@
+
+
+#!/usr/bin/python
+# Name: Anna Trostnikova
+# Section: 1
+# Date:12/07/2013
+# hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+import math
+print "********** Exercise 1.2 **********"
+print "  |  | "
+print"---------"
+print "  |  | "
+print"---------"
+print "  |  | "
+print "********** Exercise 1.3 **********"
+
+# Do your work for Excercise 1.3 here. Hint - how many different
+# variables will you need?
+
+a = "  |  | "
+b = "---------"
+print a
+print b
+print a
+print b
+print a
+
+
+
+
+print "********** Exercise 1.4 **********"
+print "********* Part II *************"
+a = (3*5) / (2+3)
+b = math.sqrt(7+9)*2
+c = (4-7)**3
+#d =(7+9)**2*0.5 
+#g = 
+h = 6 % 4
+
+
+
+print a,b,c,h
+
+print "********* Part III *************"
+a = (2+5)**8
+b = 2+5**8
+print a,b
+
+
+
+print "********** Exercise 1.5 **********"
+
+print "Not implemented" # Delete this line when you write your code!
